d5884eebdf8545ae800059b7cefc990a68525017
ThaoHienPhan <hien096456789@gmail.com>
2024-04-08 22:44:09 +0700

    Implement waiting room UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Content, MenuBar } from "@/components/moleculers";
+import Waiting from "./waiting/page";
 
 export default function Home() {
   return (
-    <div className="flex h-screen gap-3 overflow-hidden p-5">
-      <Content />
+    // <div className="flex h-screen gap-3 overflow-hidden p-5">
+    //   {/* <Content /> */}
+    // </div>
+    <div>
+      <Waiting />
     </div>
   );
 }
--- app/waiting/page.tsx
+++ app/waiting/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { ControlMenu } from "@/components/moleculers";
+import React from "react";
 
 const Waiting = () => {
   return (
-    <div>Waiting</div>
-  )
-}
+    <div className="flex w-screen h-screen">
+      <div className="w-1/2 bg-slate-500">LINK</div>
+      <div className="w-1/2 bg-slate-600 flex flex-col">
+          <div className="min-h-[91%] flex items-center justify-center">Avatar</div>
+          <div>
+            <ControlMenu />
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export default Waiting
+export default Waiting;
