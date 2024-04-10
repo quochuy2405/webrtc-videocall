ec0cfcb15fe7dfe5cb5b11a386545aae29bd6410
quochuy2405 <huy01682312920@gmail.com>
2024-04-10 22:59:14 +0700

    fix bug cho ba hien

--- app/waiting/page.tsx
+++ app/waiting/page.tsx
@@ -1,22 +1,47 @@
-"use client"
+"use client";
 
 import { ControlMenu } from "@/components/moleculers";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Waiting = () => {
+  const refVideo = useRef<HTMLVideoElement | null>(null);
 
-  const onOpenCam = (camState:boolean) => {
-    console.log('cam', camState)
-  }
+  useEffect(() => {
+    const initializeCamera = async () => {
+      try {
+        const source = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+        if (refVideo.current) refVideo.current.srcObject = source;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    initializeCamera();
+  }, []);
+
+  const onOpenCam = (camState: boolean) => {
+    console.log("cam", camState);
+  };
 
   return (
-    <div className="flex w-screen h-screen">
+    <div className="flex h-screen">
       <div className="w-1/2 bg-slate-500">LINK</div>
-      <div className="w-1/2 bg-slate-600 flex flex-col">
-          <div className="min-h-[91%] flex items-center justify-center">Avatar</div>
-          <div>
-            <ControlMenu onOpenCam={onOpenCam}/>
-          </div>
+      <div className="flex w-1/2 flex-col bg-slate-600">
+        <div className="flex flex-1 items-center justify-center">
+          <video
+            ref={refVideo}
+            autoPlay
+            playsInline
+            muted
+            className="h-full w-full bg-black"
+          ></video>
+        </div>
+        <div>
+          <ControlMenu onOpenCam={onOpenCam} />
+        </div>
       </div>
     </div>
   );
